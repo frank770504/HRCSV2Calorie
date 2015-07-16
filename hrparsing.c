f74433bdae4f1ba5f1dbc213f722bcec8150b405
frank.yw.chen <frank.yw.chen@acer.com>
2015-07-16 10:57:56 +0800

    read csv draft

---
+++ hrparsing.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+ 
+const char* getfield(char* line, int num)
+{
+	const char* tok;
+	for (tok = strtok(line, ";");
+			tok && *tok;
+			tok = strtok(NULL, ";\n"))
+	{
+		if (!--num)
+			return tok;
+	}
+	return NULL;
+}
+ 
+int main()
+{
+	FILE *f;
+	f = fopen("./test.csv", "r");
+	char line[1024];
+	while (fgets(line, 1024, f))
+	{
+		char* tmp = strdup(line);
+		printf("%s\n", tmp);
+		printf("Field 3 would be %s\n", getfield(tmp, 3));
+		// NOTE strtok clobbers tmp
+		free(tmp);
+	}
+}
